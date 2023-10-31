661f1912524dbd1b79631ad87324f0bc98804edf
Shiguang Wang <sgwang@meta.com>
2023-10-31 00:14:32 -0700

    Add the a.vim to enable ":A" to quick switch source and header files for C/C++

--- nvim/lua/me/plugins/init.lua
+++ nvim/lua/me/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigator
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigator
+	"vim-scripts/a.vim", -- a.vim script to quickly switch header/source file
 }
